a2eb3302e30d5a7fbb2ffe61ce4a711175214dc4
Rishabh Singh <rishabh.si@browserstack.com@Rishabhs-MacBook-Air.local>
2025-06-26 16:09:21 +0530

    uncommented importsnt lines

--- test/home.spec.js
+++ test/home.spec.js
@@ -11,23 +11,23 @@ describe('Functional Onboarding Testing', function() {
     browser.navigateTo('/').assert.titleEquals("Automation Practice - Ultimate QA")
   })
 
-//   it('Checking all Links are Working in the page', function(browser){
-//     const linksToTest = [
-//     { selector: 'Big page with many elements', expectedUrlPath: 'https://ultimateqa.com/complicated-page', expectedTitle: 'Complicated Page - Ultimate QA' },
-//     { selector: 'Fake Landing Page', expectedUrlPath: 'https://ultimateqa.com/fake-landing-page', expectedTitle: 'Fake landing page - Ultimate QA' },
-//     { selector: 'Fake Pricing Page', expectedUrlPath: 'https://ultimateqa.com/automation/fake-pricing-page/', expectedTitle: 'Fake pricing page - Ultimate QA' },
-//     { selector: 'Fill out forms', expectedUrlPath: 'https://ultimateqa.com/filling-out-forms/', expectedTitle: 'Filling Out Forms - Ultimate QA' },
-//     { selector: 'Learn how to automate an application that evolves over time', expectedUrlPath: 'https://ultimateqa.com/sample-application-lifecycle-sprint-1/', expectedTitle: 'Sample Application Lifecycle - Sprint 1 - Ultimate QA' },
-//     { selector: 'Login automation', expectedUrlPath: 'https://courses.ultimateqa.com/users/sign_in', expectedTitle: 'Sign In - UltimateQA' },
-//     { selector: 'Interactions with simple elements', expectedUrlPath: 'https://ultimateqa.com/simple-html-elements-for-automation/', expectedTitle: 'Simple HTML Elements For Automation - Ultimate QA' }
-// ];
-//     for( const link of linksToTest){
-//         browser.navigateTo('/').element.findByText(link.selector).click()
+  it('Checking all Links are Working in the page', function(browser){
+    const linksToTest = [
+    { selector: 'Big page with many elements', expectedUrlPath: 'https://ultimateqa.com/complicated-page', expectedTitle: 'Complicated Page - Ultimate QA' },
+    { selector: 'Fake Landing Page', expectedUrlPath: 'https://ultimateqa.com/fake-landing-page', expectedTitle: 'Fake landing page - Ultimate QA' },
+    { selector: 'Fake Pricing Page', expectedUrlPath: 'https://ultimateqa.com/automation/fake-pricing-page/', expectedTitle: 'Fake pricing page - Ultimate QA' },
+    { selector: 'Fill out forms', expectedUrlPath: 'https://ultimateqa.com/filling-out-forms/', expectedTitle: 'Filling Out Forms - Ultimate QA' },
+    { selector: 'Learn how to automate an application that evolves over time', expectedUrlPath: 'https://ultimateqa.com/sample-application-lifecycle-sprint-1/', expectedTitle: 'Sample Application Lifecycle - Sprint 1 - Ultimate QA' },
+    { selector: 'Login automation', expectedUrlPath: 'https://courses.ultimateqa.com/users/sign_in', expectedTitle: 'Sign In - UltimateQA' },
+    { selector: 'Interactions with simple elements', expectedUrlPath: 'https://ultimateqa.com/simple-html-elements-for-automation/', expectedTitle: 'Simple HTML Elements For Automation - Ultimate QA' }
+];
+    for( const link of linksToTest){
+        browser.navigateTo('/').element.findByText(link.selector).click()
         
-//         browser.assert.titleEquals(link.expectedTitle);
-//         browser.assert.urlEquals(link.expectedUrlPath);
-//     }
-//   })
+        browser.assert.titleEquals(link.expectedTitle);
+        browser.assert.urlEquals(link.expectedUrlPath);
+    }
+  })
 
 
 
